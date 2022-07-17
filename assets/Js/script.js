ce287672fc4c272f353b457479d16f3cd9848a0b
Donya <thisis2nya@gmail.com>
2022-07-17 20:46:55 +0430

    Bugs fixed / All filters apply at the same time

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,92 +1,108 @@
 const image = document.querySelector("#image");
+const saveBtn = document.querySelector(".save");
+const rangeInputs = document.querySelectorAll(".range");
+
+let brightness = 29;
+let Blur = 0;
+let contrast = 50;
+let grayscale = 0;
+let hue = 0;
+let invert = 0;
+let opacity = 100;
+let saturate = 0;
+let sepia = 0;
 
 // Radius
 const radiusRange = document.querySelector(".radius-range");
 const radiusValue = document.querySelector(".radius-value");
 function setRadiusValue(e) {
-    radiusRange.value = (e.target.value<51) ? e.target.value : 50;
-    const value = e.target.value;
+    const value = Number(e.target.value);
     radiusValue.innerText = `${value}%`;
-    image.style.borderRadius = `${value}%`;
+    // Checks if square
+    image.style.borderRadius = (image.width === image.height) ? `${value/2}%` : `${value}px`
 }
 
 // Brightness
 const brightnessRange = document.querySelector(".brightness-range");
 const brightnessValue = document.querySelector(".brightness-value");
 function setBrightnessValue(e) {
-    const value = e.target.value;
-    brightnessValue.innerText = `${value}%`;
-    image.style.filter = `brightness(${value * 2}%)`;
+    brightness = Number(e.target.value);
+    brightnessValue.innerText = `${brightness}%`;
 }
 
 // Blur
 const blurRange = document.querySelector(".blur-range");
 const blurValue = document.querySelector(".blur-value");
 function setBlurValue(e) {
-    const value = e.target.value;
-    blurValue.innerText = `${value}%`;
-    image.style.filter = `blur(${value/4}px)`;
+    Blur = Number(e.target.value);
+    blurValue.innerText = `${Blur}%`;
 }
 
 // Contrast
 const contrastRange = document.querySelector(".contrast-range");
 const contrastValue = document.querySelector(".contrast-value");
 function setContrastValue(e) {
-    const value = e.target.value;
-    contrastValue.innerText = `${value}%`;
-    image.style.filter = `contrast(${value * 2}%)`;
+    contrast = Number(e.target.value);
+    contrastValue.innerText = `${contrast}%`;
+
 }
 
 // Grayscale
 const grayscaleRange = document.querySelector(".grayscale-range");
 const grayscaleValue = document.querySelector(".grayscale-value");
 function setGrayscaleValue(e) {
-    const value = e.target.value;
-    grayscaleValue.innerText = `${value/100}`;
-    image.style.filter = `grayscale(${value/100})`;
+    grayscale = Number(e.target.value);
+    grayscaleValue.innerText = `${grayscale/100}`;
 }
 
 // Hue
 const hueRange = document.querySelector(".hue-range");
 const hueValue = document.querySelector(".hue-value");
 function setHueValue(e) {
-    const value = e.target.value;
-    hueValue.innerText = `${value}deg`;
-    image.style.filter = `hue-rotate(${value * 5}deg)`;
+    hue = Number(e.target.value);
+    hueValue.innerText = `${hue}deg`;
 }
 
 // Invert
 const invertRange = document.querySelector(".invert-range");
 const invertValue = document.querySelector(".invert-value");
 function setInvertValue(e) {
-    const value = e.target.value;
-    invertValue.innerText = `${value/100}`;
-    image.style.filter = `invert(${value/100})`;
+    invert = Number(e.target.value);
+    invertValue.innerText = `${invert/100}`;
 }
 
 // Opacity
 const opacityRange = document.querySelector(".opacity-range");
 const opacityValue = document.querySelector(".opacity-value");
 function setOpacityValue(e) {
-    const value = e.target.value;
-    opacityValue.innerText = `${value/100}`;
-    image.style.filter = `opacity(${value/100})`;
+    opacity = Number(e.target.value);
+    opacityValue.innerText = `${opacity/100}`;
 }
 
 // Saturate
 const saturateRange = document.querySelector(".saturation-range");
 const saturateValue = document.querySelector(".saturation-value");
 function setSaturateValue(e) {
-    const value = e.target.value;
-    saturateValue.innerText = `${value}%`;
-    image.style.filter = `saturate(${value+1})`;
+    saturate = Number(e.target.value);
+    saturateValue.innerText = `${saturate}%`;
 }
 
 // Sepia
 const sepiaRange = document.querySelector(".sepia-range");
 const sepiaValue = document.querySelector(".sepia-value");
 function setSepiaValue(e) {
-    const value = e.target.value;
-    sepiaValue.innerText = `${value/100}`;
-    image.style.filter = `sepia(${value/100})`;
+    sepia = Number(e.target.value);
+    sepiaValue.innerText = `${sepia/100}`;
 }
+
+function updateFilters() {
+
+    image.style.webkitFilter = `brightness(${brightness*2}%) blur(${Blur/10}px) contrast(${contrast*2}%) grayscale(${grayscale/100}) hue-rotate(${hue*5}deg) invert(${invert/100}) opacity(${opacity/100}) saturate(${saturate+1}) sepia(${sepia/100})`;
+
+    image.style.filter = `brightness(${brightness*2}%) blur(${Blur/4}px) contrast(${contrast*2}%) grayscale(${grayscale/100}) hue-rotate(${hue*5}deg) invert(${invert/100}) opacity(${opacity/100}) saturate(${saturate+1}) sepia(${sepia/100})`;
+
+}
+
+rangeInputs.forEach(input => {
+    input.addEventListener("input", updateFilters)
+})
